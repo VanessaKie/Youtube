40eff1a62c80bc876adbc5c139502f03f5d82415
Vanessa Kienzl <vanessa.kienzl@a1.at>
2020-01-19 13:56:01 +0100

    changed loader

--- src/Components/VideoDetail/index.js
+++ src/Components/VideoDetail/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { css } from "@emotion/core";
-import { RingLoader } from "react-spinners";
+import { CircleLoader} from "react-spinners";
 
 const VideoDetail = (props) => {
 
     return(          
         <>
-        {props.loading ? <div className="loader"> <RingLoader css={css} size={150} color={"#AFDCF1"} loading={props.loading} /> </div> : <div className='col-7 player p-2'> <iframe className="text-center p-2" id="vid" width="640" height="360" src= {`http://www.youtube.com/embed/${props.iFrame}?autoplay=1`} frameborder="0" allowFullScreen/>
+        {props.loading ? <div className="loader"> <CircleLoader
+ css={css} size={150} color={"#AFDCF1"} loading={props.loading} /> </div> : <div className='col-7 player p-2'> <iframe className="text-center p-2" id="vid" width="640" height="360" src= {`http://www.youtube.com/embed/${props.iFrame}?autoplay=1`} frameborder="0" allowFullScreen/>
             <h4 className='p-3 text-left'>{props.playTitle}</h4>
             <p className="p-3 text-left">{props.playDescription}</p>
            </div>}
